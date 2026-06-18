95bf0c8589cd15205702b853642b1ea009a209a9
Urjeshadeloitte <urbakshi@deloitte.com>
2026-06-18 22:56:04 +0530

    Delete weather directory

--- weather/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Package initialization for weather forecasting."""
-
-__version__ = "0.1.0"
-
-from weather.graph import graph  # noqa: F401
-from weather.state import PipelineState  # noqa: F401
